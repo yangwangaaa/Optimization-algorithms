6de6e15370714c8083f4b8691805dcfcc56cff7f
WardQ <ward.quaghebeur@gmail.com>
2018-03-29 15:45:42 -0400

    added result SCE

--- Shuffled Complex Evolution/script.m
+++ Shuffled Complex Evolution/script.m
@@ -5,7 +5,8 @@ ParSCE.q = 3;
 ParSCE.alfa = 5;
 ParSCE.beta = 5;
 ParSCE.factor = .9;
-ParSCE.threshold = 0.05;
+ParSCE.threshold = 0.5;
 ParSCE.maxit = 50;
 
-[xStar,iteratiematrix, populatiematrix] = SCE(@alpine, [0 100], ParSCE)
+[xStar,iteratiematrix, populatiematrix] = SCE(@alpine, [-20 20; -20 20], ParSCE);
+plotPopulatie(@alpine, [-20 20; -20 20], populatiematrix)
